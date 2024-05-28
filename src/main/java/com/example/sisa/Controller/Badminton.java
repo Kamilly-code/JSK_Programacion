10ed66eef367090ad243455cabf4da86e9ccd24f
ismael.sisa <ismael.sisa@educa.madrid.org>
2024-05-28 09:11:03 +0200

    28/05/2024

--- src/main/java/com/example/sisa/Controller/Badminton.java
+++ src/main/java/com/example/sisa/Controller/Badminton.java
@@ -12,32 +12,63 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Badminton implements Initializable {
+
+    /**
+     * Panel principal.
+     */
+
     @FXML
     private AnchorPane anchorPane;
+
+
+    /**
+     * Maneja el evento de clic para mostrar la vista de Badminton1.
+     *
+     * @param event El evento de acción que se dispara al hacer clic.
+     */
+
+
     @FXML
-    private void verMasBadminton1(ActionEvent event){
+    private void verMasBadminton1(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/sisa/Badminton1.fxml"));
-            Parent baloncesto1 = loader.load();
-
-            anchorPane.getChildren().setAll(baloncesto1);
+            Parent badminton1 = loader.load();
+            anchorPane.getChildren().setAll(badminton1);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
+
+    /**
+     * Maneja el evento de clic para mostrar la vista de Badminton2.
+     *
+     * @param event El evento de acción que se dispara al hacer clic.
+     */
+
+
     @FXML
-    private void verMasBadminton2(ActionEvent event){
+    private void verMasBadminton2(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/sisa/Badminton2.fxml"));
-            Parent baloncesto2 = loader.load();
-
-            anchorPane.getChildren().setAll(baloncesto2);
+            Parent badminton2 = loader.load();
+            anchorPane.getChildren().setAll(badminton2);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
+    /**
+     * Inicializa el controlador. Este método se llama automáticamente después de cargar el archivo FXML.
+     *
+     * @param url La ubicación utilizada para resolver rutas relativas del objeto raíz o null si la ubicación no es conocida.
+     * @param resourceBundle Los recursos utilizados para localizar el objeto raíz o null si el objeto raíz no fue localizado.
+     */
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        // Inicialización del controlador, si es necesario.
     }
 }
